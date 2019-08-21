18664550b9740fe206384f793c501c2a9d36760b
Hidrow <54356633+Hidrow@users.noreply.github.com>
2019-08-21 07:53:44 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Enari_Classe_Usuario
+{
+    class Usuario
+    {
+        //Atributos
+        private string nome, sobrenome, login, senha, email;
+
+
+        public string Getnome()
+        {
+            return nome;
+        }
+
+        public string Getsobrenome()
+        {
+            return sobrenome;
+        }
+
+        public string Getlogin()
+        {
+            return login;
+        }
+
+        public string Getsenha()
+        {
+            return senha;
+        }
+
+        public string Getemail()
+        {
+            return email;
+        }
+
+        public void Setnome(string x)
+        {
+            nome = x;
+        }
+
+        public void Setsobrenome(string x)
+        {
+            sobrenome = x;
+        }
+
+        public void Setlogin(string x)
+        {
+            login = x;
+        }
+
+        public void Setsenha(string x)
+        {
+            senha = x;
+        }
+
+        public void Setemail(string x)
+        {
+            email = x;
+        }
+
+    }
+     
+}
+
+
